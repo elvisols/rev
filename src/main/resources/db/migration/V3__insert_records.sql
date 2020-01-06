94de4150876fc07585311dd0a15b5a051653b970
Elvis O <elviso@activedgetechnologies.com>
2020-01-06 18:14:10 +0100

    refactored migration scripts to insert accounts

--- src/main/resources/db/migration/V3__insert_records.sql
+++ src/main/resources/db/migration/V3__insert_records.sql
@@ -1,3 +1,8 @@
 INSERT INTO rev_db.customer VALUES (next value for rev_db.s_customer_id, 'John Doe', 'john@email.com', '08043452384', null, '1934-12-10', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP );
 INSERT INTO rev_db.customer VALUES (next value for rev_db.s_customer_id, 'Ben Liz', 'ben@email.com', '08040452311', null, '1994-11-11', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP );
 INSERT INTO rev_db.customer VALUES (next value for rev_db.s_customer_id, 'Ann Foe', 'ann@email.com', '08043422224', 'My little address', '1945-02-08', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP );
+
+INSERT INTO rev_db.account VALUES (next value for rev_db.s_account_id, 1, '1111111111', 'Acc One', 'Regular', '08000000000', 50000, 'USD', 'bc123', 1, CURRENT_TIMESTAMP);
+INSERT INTO rev_db.account VALUES (next value for rev_db.s_account_id, 2, '2222222222', 'Acc Two', 'Regular', '08000000000', 50000, 'USD', 'bc123', 1, CURRENT_TIMESTAMP);
+INSERT INTO rev_db.account VALUES (next value for rev_db.s_account_id, 3, '3333333333', 'Acc Three', 'Regular', '08000000000', 50000, 'USD', 'bc123', 1, CURRENT_TIMESTAMP);
+INSERT INTO rev_db.account VALUES (next value for rev_db.s_account_id, 2, '4444444444', 'Acc Four', 'Foreign', '08000000000', 70000, 'USD', 'bc123', 1, CURRENT_TIMESTAMP);
